fb7e90ed3c9970c4c4f6399e6fe345246ccd655c
rodrigo-marcolino <rodrigomarcolino@gmail.com>
2023-04-16 13:29:49 +1200

    feat: add  put method update() Controler

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -33,7 +33,20 @@ public IActionResult Create(Pizza pizza)
     return CreatedAtAction(nameof(Get), new { id = pizza.Id }, pizza);
 }
 
-    // PUT action
+   [HttpPut("{id}")]
+public IActionResult Update(int id, Pizza pizza)
+{
+    if (id != pizza.Id)
+        return BadRequest();
+           
+    var existingPizza = PizzaService.Get(id);
+    if(existingPizza is null)
+        return NotFound();
+   
+    PizzaService.Update(pizza);           
+   
+    return NoContent();
+}
 
     // DELETE action
 }
